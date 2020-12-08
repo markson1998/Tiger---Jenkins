093b95970fb0ce17148a286ff25a055eca159639
Markson Leite <m.oliveira-leite@mycit.ie>
2020-12-08 17:11:32 +0000

    Failed

--- sample.py
+++ sample.py
@@ -8,8 +8,8 @@ List2.sort()
 print("The sorted list is", List2)
 
 List3 = []
-List3 = List1.copy()
+List3  List1.copy()
 print("List3 =", List3)
 
 indexvalue = List2[2:6]
-print("The index values are", indexvalue)
+print("The index values are = ", indexvalue)
